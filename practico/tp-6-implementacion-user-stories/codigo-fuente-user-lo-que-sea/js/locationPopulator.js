15fec8ac432c319457725d5ba8ebdc3e1ec03b7d
Francisco de Maussion <franciscodemaussion@gmail.com>
2020-09-03 13:01:40 -0300

    Dynamic load deptos, Moves js files to js/

---
+++ practico/tp-6-implementacion-user-stories/codigo-fuente-user-lo-que-sea/js/locationPopulator.js
@@ -0,0 +1,11 @@
+$(function() {
+    // var data = ["Cordoba", "Rio Cuarto", "Villa Maria", "San Francisco", "Alta Gracia", "Rio Tercero", "La Calera"];
+    $.get("https://apis.datos.gob.ar/georef/api/departamentos?provincia=14&orden=id&campos=completo&formato=json",
+    function(data, status){
+        console.log(data.departamentos);
+        $.each(data.departamentos, function (i, depto) {
+            console.log(depto.id, i);
+            $("#inputCity").append($('<option>', {value: i, text: depto.nombre}));
+        });
+    });
+});
